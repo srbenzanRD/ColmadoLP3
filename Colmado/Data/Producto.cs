59e0fca9c16dad4c479e1da3802a3cb94a64110d
Felix Benzan <felixbenz07@gmail.com>
2023-04-28 20:39:07 -0400

    Iniciacion de entidad productos

---
+++ Colmado/Data/Producto.cs
@@ -0,0 +1,9 @@
+namespace Colmado.Data;
+
+public class Producto
+{
+    public string Descripcion {get; set;}
+    public float Cantidad {get; set;}
+    public decimal Precio {get; set;}
+    public decimal SubTotal => Cantidad * Precio;
+}
