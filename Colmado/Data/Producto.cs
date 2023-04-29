0d16675195d4830aaaa6e9e0a2a29a67453dd2bc
Felix Benzan <felixbenz07@gmail.com>
2023-04-28 20:50:11 -0400

    28_04_2023

--- Colmado/Data/Producto.cs
+++ Colmado/Data/Producto.cs
@@ -5,5 +5,5 @@ public class Producto
     public string Descripcion {get; set;}
     public float Cantidad {get; set;}
     public decimal Precio {get; set;}
-    public decimal SubTotal => Cantidad * Precio;
+    public decimal SubTotal => (decimal)Cantidad * Precio;
 }
